2bc11d0e8a3f04c8f3752af00bcbf61b1dcc1c0f
wuhaowen <wuhaowen0731@163.com>
2016-03-17 23:24:20 +0800

    删除多余文件

--- Encryptor/build/intermediates/ndk/debug/obj/local/armeabi/objs/signature-encryptor/Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.o.d
+++
@@ -1,2 +0,0 @@
-/Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/build/intermediates/ndk/debug/obj/local/armeabi/objs/signature-encryptor//Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.o: \
- /Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.c
--- Encryptor/build/intermediates/ndk/debug/obj/local/mips64/objs/signature-encryptor/Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.o.d
+++
@@ -1,2 +0,0 @@
-/Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/build/intermediates/ndk/debug/obj/local/mips64/objs/signature-encryptor//Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.o: \
- /Users/wuhaowen/Documents/Workspace/Android/SignatureEncryptor/Encryptor/src/main/jni/dummy.c
